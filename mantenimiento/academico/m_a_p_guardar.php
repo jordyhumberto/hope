4cf081ac597bdc707a0659c25c0b8f627df48cc1
jordy villavicencio <jordyhvillavicencio@gmail.com>
2018-08-22 16:39:38 -0500

    plan ya

---
+++ mantenimiento/academico/m_a_p_guardar.php
@@ -0,0 +1,46 @@
+<?php
+	session_start(); //Inicia una nueva sesión o reanuda la existente
+	require '../../conexion.php'; //Agregamos el script de Conexión
+	if(!isset($_SESSION["id_usuario"])){
+		header("Location: ../../index.php");
+	}
+    $id=$_POST['id'];
+    $descripcion=$_POST['descripcion'];
+    $estado=$_POST['estado'];
+   
+	$sql = "INSERT INTO tbl_plan(IDPlan,Descripcion,Estado) VALUES ('$id','$descripcion','$estado')";
+	$resultado = $mysqli->query($sql);
+?>
+
+<html lang="es">
+	<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<meta name="keywords" content="universidad, peruana, investigación, investigacion, negocios, upein, UPEIN">
+		<meta name="description" content="UPEIN! - Universidad Peruana de Invesitgacion y Negocios da la bienvenida a sus nuevos estudiantes">
+		<title>Intranet</title>
+		<link href="../../img/favicon.ico" rel="shortcut icon" type="image/x-icon">
+		<link href="../../css/bootstrap.min.css" rel="stylesheet">
+		<link href="../../css/bootstrap-theme.css" rel="stylesheet">
+		<script src="../../js/jquery-3.3.1.min.js"></script>
+		<script src="../../js/bootstrap.min.js"></script>	
+	</head>
+	
+	<body>
+		<div class="container">
+			<div class="row">
+				<div class="row" style="text-align:center">
+                    <?php 
+                        if($resultado) { 
+						    echo '<h3>REGISTRO GUARDADO</h3>';
+						} else { 
+						    echo '<h3>ERROR AL GUARDAR</h3>';
+                        } 
+                    ?>
+					<a href="m_a_plan.php" class="btn btn-primary">Regresar</a>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
